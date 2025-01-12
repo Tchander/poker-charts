8b17ee172b93d4d87c4f3c9c5f04f533a5e134b1
Evgeniy Chander <evgeniy.chander@maximumtest.ru>
2025-01-12 14:57:01 +0300

    pc-5: created chart interfaces

--- src/router/enums.ts
+++ src/router/enums.ts
@@ -1,3 +1,4 @@
 export enum RouteName {
   HOME = 'home',
+  TEST_PAGE = 'testPage',
 }
--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -1,10 +1,15 @@
-import type { RouteRecordRaw } from 'vue-router';
 import { RouteName } from '@/router/enums';
+import type { RouteRecordRaw } from 'vue-router';
 
 export const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: RouteName.HOME,
     component: () => import('@/pages/HomePage.vue'),
+  },
+  {
+    path: '/test-page',
+    name: RouteName.TEST_PAGE,
+    component: () => import('@/pages/TestPage.vue'),
   },
 ];
